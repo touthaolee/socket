32268dcdc53b1cd54df6dc46b73d7f5d1a46f50e
touthaolee <touthao.lee@gmail.com>
2025-05-06 16:21:20 -0700

    Robust Authentication system

--- client-side/client-modules/socket-module.js
+++ client-side/client-modules/socket-module.js
@@ -1,5 +1,5 @@
 import { getAuthToken } from './auth-module.js';
-import { showToast } from '../client-utils/client-helpers.js';
+import { showToast } from '../client-utils/ui-utils.js';
 import { getTokenFromStorage } from '../client-utils/storage-utils.js';
 
 // ...existing code...
